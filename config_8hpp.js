32fc3524e2a6dfefede0e9a774474c0f62011dee
Documentation Builder <travis@travis-ci.org>
2015-01-26 13:29:31 +0000

    Automated documentation build for changeset 2a1fd92ab4748d52298eea2ddaf67b725f30c91f.

--- config_8hpp.js
+++ config_8hpp.js
@@ -4,5 +4,6 @@ var config_8hpp =
     [ "option_error", "classConfig_1_1option__error.html", "classConfig_1_1option__error" ],
     [ "ConfigOption", "classConfig_1_1ConfigOption.html", "classConfig_1_1ConfigOption" ],
     [ "ConfigParser", "classConfig_1_1ConfigParser.html", "classConfig_1_1ConfigParser" ],
-    [ "configPrefixData", "config_8hpp.html#a0f82b307506f886adc0d12980637eee2", null ]
+    [ "configPrefixData", "config_8hpp.html#a0f82b307506f886adc0d12980637eee2", null ],
+    [ "configPrefixFile", "config_8hpp.html#a61850ec3e8ecbe7c23a942b6de21b1be", null ]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['configprefixdata',['configPrefixData',['../namespaceConfig.html#a0f82b307506f886adc0d12980637eee2',1,'Config']]],
+  ['configprefixfile',['configPrefixFile',['../namespaceConfig.html#a61850ec3e8ecbe7c23a942b6de21b1be',1,'Config']]],
   ['creator',['creator',['../classfileChannel_1_1FileChannel.html#a2f04a5ef7bce871eeb58c814eb3db4eb',1,'fileChannel::FileChannel::creator()'],['../classircChannel_1_1IrcChannel.html#a263dc2abd533b96a31f91f92eaa7f71f',1,'ircChannel::IrcChannel::creator()']]]
 ];
