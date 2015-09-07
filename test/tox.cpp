513649f71a8c7fc0b16bafc4a6bf24d44a8c679b
Slava Barinov <rayslava@gmail.com>
2015-09-07 09:46:03 +0300

    [Test] Tox address conversion test added
    Signed-off-by: Slava Barinov <rayslava@gmail.com>

--- test/tox.cpp
+++ test/tox.cpp
@@ -1,5 +1,26 @@
 #include "../src/channel.hpp"
 #include <gtest/gtest.h>
+#include <tox/tox.h>
+
+namespace toxChannel {
+  namespace util {
+      template <size_t Size>
+      std::string ToxId2HR(const uint8_t* address);
+  }
+}
+
+TEST(ToxUtil, hex2bin)
+{
+    const uint8_t toxIdHex[TOX_ADDRESS_SIZE] = {
+	0x96, 0x18, 0xfc, 0x56, 0x1e, 0x9e, 0xd1, 0xa1, 0xb3, 0x72, 0x62, 0x63,
+	0x8a, 0x50, 0xb2, 0x5c, 0x6e, 0x5e, 0xad, 0x39, 0x43, 0xb9, 0x39, 0x4f,
+	0x78, 0x69, 0x28, 0x0b, 0xb2, 0x69, 0x46, 0x36, 0x49, 0x69, 0x0c, 0x9c,
+	0x14, 0x35};
+
+    const std::string toxIdStr = "9618FC561E9ED1A1B37262638A50B25C6E5EAD3943B9394F7869280BB269463649690C9C1435";
+    const auto converted = toxChannel::util::ToxId2HR<TOX_ADDRESS_SIZE>(toxIdHex);
+    ASSERT_STREQ(converted.c_str(), toxIdStr.c_str());
+}
 
 TEST(ToxChannel, name)
 {
